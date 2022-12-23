b8b1d30f16e7587c395f0144add44079e4beb2aa
Moo Plah Soe Kusay <kusaymoo@ecs.vuw.ac.nz>
2022-12-23 14:22:18 +1300

    registration: add sign-in-page component.

---
+++ src/components/signin-page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { GoogleButton } from "react-google-button";
+
+export default function SignIn() {
+  return (
+    <>
+      <div className="App" id="user">
+        <Card
+          className="text-secondary mt-4"
+          style={{
+            width: 350,
+            fontSize: "18px",
+            textAlign: "left",
+          }}
+        >
+          <Card.Body style={{ alignItems: "center" }}>
+            <Card.Title>Sign In.</Card.Title>
+            <Card.Text>
+              Please press the button below to register a Vexed account, or sign
+              in to an existing one.
+            </Card.Text>
+            <GoogleButton style={{ width: "100%" }} type="light"></GoogleButton>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
+}
