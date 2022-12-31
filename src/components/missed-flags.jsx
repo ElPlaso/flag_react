8ac05e95690950decdc96110985cbc9be484ce7a
Moo Plah Soe Kusay <kusaymoo@ecs.vuw.ac.nz>
2022-12-31 22:48:13 +1300

    game-ui: update flag size.

--- src/components/missed-flags.jsx
+++ src/components/missed-flags.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import FlagView from "./flag";
 import Carousel from "react-bootstrap/Carousel";
+import { Card } from "react-bootstrap";
 
 export default function MissedFlags(props) {
   const { flags } = props;
   return (
     <div>
-      <Carousel variant="dark" className="mt-4 ">
+      <Carousel variant="dark" className="mt-4 " style={{ width: 300 }}>
         {flags.map((country) => {
           return (
             <Carousel.Item key={country.code}>
-              <FlagView name={country.code} />
-              <div className="text-secondary">{country.name}</div>
+              <Card
+                style={{
+                  justifyContent: "center",
+                  height: "320px",
+                  backgroundColor: "transparent",
+                }}
+              >
+                <FlagView name={country.code} />
+                <div className="text-secondary">{country.name}</div>
+              </Card>
             </Carousel.Item>
           );
         })}
